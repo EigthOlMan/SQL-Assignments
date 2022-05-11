0f09b163b1d6e3447dab6bf0d385f6dd87bf671e
EigthOlMan <dralcubierre1@gmail.com>
2022-05-11 00:09:06 -0500

    Create Assignment_1.sql

---
+++ Assignment_1.sql
@@ -0,0 +1,80 @@
+--Henry Dykhuizen 
+
+--05/01/2022
+
+----------------Conceptual Questions-----------------------------
+--1. CRUD stands for Create, Read, Update, Delete
+--2. The System Databases are master, model, msdb, tempdb 
+--   They are important databases that the DBMS needs to 
+--   operate.
+--3. OLAP is Online Analytic Process, and they deal with m ore
+--   memory and handle analytics on large datasets and histories.
+--   OLTP is  Online Transaction Process, and they deal with the tranascation 
+--   that are less memory intensive than the needs of an OLAP.
+--4. The difference of between Data Definition Language
+--   and Data Maintenance Language is that DDL establishes the
+--   DB schema and DML is used to access or modify the data.
+--5. The Like keyword is used with wildcards to
+--	 access records tha match a specified pattern
+--6. The wild cards available to the like keyword are
+--		% zero or more characters
+--		_ single character
+--		[] single character in the brackets
+--		^ any character not in brackets
+--		- single character within the range of
+--7. The difference between the left join and inner join
+--	 is that the inner join only shows records with corrisponding 
+--	 datum in both tables, while left join will show all records of
+--	 the left table regardless if it has a corresponding record in
+--   the right.
+
+
+-------------------------Queries----------------------------------
+
+--1.
+
+Use AdventureWorks2019
+
+Go
+
+Select ProductID, Name, Color, ListPrice
+From Production.Product
+
+Go
+
+--2.
+
+Select ProductID, Name, Color, ListPrice
+From Production.Product
+Where ListPrice <> 0
+
+--3.
+
+Select ProductID, Name, Color, ListPrice
+From Production.Product
+Where Color Is Not NULL
+
+--4.
+
+Select ProductID, Name, Color, ListPrice
+From Production.Product
+Where ListPrice > 0 and Color Is Not NULL
+
+--5.
+Select Name + ' '+ Color as [Name Color]
+From Production.Product
+Where Color Is Not NULL
+
+--6.
+
+Select Name, Color
+From Production.Product
+Where Name Like '%Crankarm' Or Name like '%Chainring%'
+Order By Name Desc
+
+--7
+
+
+
+
+
